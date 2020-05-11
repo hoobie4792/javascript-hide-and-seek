0a920890d77fa3369354dc9606109c70a14c60f9
github username <github email address>
2020-05-11 17:10:22 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
 
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+}
+
+function deepestChild() {
+  let element = document.getElementById('grand-node');
+  let has_child = element.children[0];
+
+  while (has_child) {
+    element = element.children[0];
+    has_child = element.children[0];
+  }
+
+  return element;
+}
+
+function increaseRankBy(n) {
+  let rankedLists = document.getElementsByClassName('ranked-list');
+
+  for (let i = 0; i < rankedLists.length; i++) {
+    let children = rankedLists[i].children;
+
+    for (let j = 0; j < children.length; j++) {
+      children[j].innerHTML = parseInt(children[j].innerHTML) + n;
+    }
+  }
+}
