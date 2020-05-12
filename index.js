5719cfb909251a58567a12cb259563dfc64b2fcb
github username <github email address>
2020-05-12 15:16:32 -0400

    Done.

--- index.js
+++ index.js
@@ -6,26 +6,23 @@ function nestedTarget() {
   return document.querySelector('#nested .target');
 }
 
-function deepestChild() {
-  let element = document.getElementById('grand-node');
-  let has_child = element.children[0];
-
-  while (has_child) {
-    element = element.children[0];
-    has_child = element.children[0];
-  }
-
-  return element;
-}
-
 function increaseRankBy(n) {
-  let rankedLists = document.getElementsByClassName('ranked-list');
-
-  for (let i = 0; i < rankedLists.length; i++) {
-    let children = rankedLists[i].children;
-
+  let lists = document.querySelectorAll('.ranked-list');
+  for (let i = 0; i < lists.length; i++) {
+    let children = lists[i].children;
     for (let j = 0; j < children.length; j++) {
       children[j].innerHTML = parseInt(children[j].innerHTML) + n;
     }
   }
+}
+
+function deepestChild() {
+  let node = document.querySelector('#grand-node');
+  let child = node.children[0];
+  while (child) {
+    node = child;
+    child = node.children[0];
+  }
+
+  return node;
 }
